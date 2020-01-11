cf7771f2d5ef1c6efa84b4c7a129b389e3557538
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-11 16:40:55 +0100

    turtle game

---
+++ work-flow/main.py
@@ -0,0 +1,15 @@
+import turtle
+
+a = 5
+print(a)
+
+b = "hello"
+
+print(b)
+
+qazi_turtle =turtle.Turtle()
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
