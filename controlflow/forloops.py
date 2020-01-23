0b3e36d2814d3f0ecab659e63f4c4dc943596f63
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-01-23 16:12:31 +0100

    wip

--- controlflow/forloops.py
+++ controlflow/forloops.py
@@ -17,3 +17,12 @@ for i in range(1, 20):
 number2 = "6,548,924,874,784"
 for i in range(0, len(number)):
     print(number2[i])
+    
+    for i in range(0, 100, 5):
+        print("i is {}".format(i))
+    
+for i in range(1,13):
+    for j in range(1,13):
+        print("{1} times {0} is {2}".format(i, j, i*j))
+    print("===============")
+    
