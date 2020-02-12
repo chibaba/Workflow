c568f901667db7aa71bf8d81aa22368f7f1e9f70
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-02-12 13:20:28 +0100

    added dat func

--- functions/dateCalc.py
+++ functions/dateCalc.py
@@ -1,1 +1,14 @@
 import time
+
+print(time.gmtime(0))
+
+# print(time.localtime())
+
+# print(time.time())
+
+time_here =time.localtime()
+
+print(time_here)
+print("year:", time_here[0],time_here.tm_year)
+print("Month:", time_here[1], time_here.tm_mon)
+print("Day:", time_here[2], time_here.tm_day)
