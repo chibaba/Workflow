4e043e29e727b622d34aabaaf0de2a43a5a694d2
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-02-12 13:42:17 +0100

    random timer

--- functions/dateCalc.py
+++ functions/dateCalc.py
@@ -1,14 +1,34 @@
+# import time
+
+# print(time.gmtime(0))
+
+# # print(time.localtime())
+
+# # print(time.time())
+
+# time_here =time.localtime()
+
+# print(time_here)
+# print("year:", time_here[0],time_here.tm_year)
+# print("Month:", time_here[1], time_here.tm_mon)
+# print("Day:", time_here[2], time_here.tm_day)
+
 import time
 
-print(time.gmtime(0))
+from time import time as my_timer
 
-# print(time.localtime())
+import random
 
-# print(time.time())
+input("press enter to start")
 
-time_here =time.localtime()
+wait_time = random.randint(1, 6)
+time.sleep(wait_time)
+start_time = my_timer()
+input("press enter to stop")
 
-print(time_here)
-print("year:", time_here[0],time_here.tm_year)
-print("Month:", time_here[1], time_here.tm_mon)
-print("Day:", time_here[2], time_here.tm_day)
+end_time = my_timer()
+
+print("Started at " + time.strftime("%X", time.localtime(start_time)))
+print("Ended at " + time.strftime("%X",time.localtime(end_time)))
+
+print("your reaction time was {} seconds".format(end_time - start_time))
