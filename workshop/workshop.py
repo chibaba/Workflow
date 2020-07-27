d9015965be1b6cb91ae2ef0fc433606070ac1426
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-27 20:04:08 +0100

    working with python list

---
+++ workshop/workshop.py
@@ -0,0 +1,8 @@
+# Open a new Jupyter Notebook.
+# Create a list and assign it to employees.
+# Create three nested lists in employees to store the information of each employee, respectively.
+# Print the employees variable.
+# Print the details of all employees in a presentable format.
+# Print only the details of Lisa Crawford.
+
+# we would start by creating a dummy list for employees
